bede2704f00be6556f7f7114a5c3c23fdcf31468
fatehiii404070-hue <fatehiii404070@gmail.com>
2026-09-04 08:29:31 +0330

    Update AlarmRescheduler.java

--- Alarm3D-Unity-Android/Assets/Plugins/Android/src/main/java/com/alarm3d/alarm/AlarmRescheduler.java
+++ Alarm3D-Unity-Android/Assets/Plugins/Android/src/main/java/com/alarm3d/alarm/AlarmRescheduler.java
@@ -1,21 +1,49 @@
 package com.alarm3d.alarm;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 
-public final class AlarmRescheduler
+public final class AlarmReceiver extends BroadcastReceiver
 {
-    private AlarmRescheduler()
-    {
-    }
-
-    public static void reschedule(Context context)
+    @Override
+    public void onReceive(
+            Context context,
+            Intent intent)
     {
         if (context == null)
         {
             return;
         }
 
-        AlarmSchedulerBridge.rescheduleAll(
-                context.getApplicationContext());
+        Intent serviceIntent =
+                new Intent(
+                        context,
+                        AlarmForegroundService.class);
+
+        if (intent != null)
+        {
+            String alarmId =
+                    intent.getStringExtra("alarm_id");
+
+            if (alarmId != null)
+            {
+                serviceIntent.putExtra(
+                        "alarm_id",
+                        alarmId);
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            context.startForegroundService(
+                    serviceIntent);
+        }
+        else
+        {
+            context.startService(
+                    serviceIntent);
+        }
     }
 }
