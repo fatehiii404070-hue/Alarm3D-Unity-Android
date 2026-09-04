df717f19b0faa412a785f83b5ee7e5479cbbe8dd
fatehiii404070-hue <fatehiii404070@gmail.com>
2026-09-04 08:18:10 +0330

    Update AlarmRescheduler.java

--- Alarm3D-Unity-Android/Assets/Plugins/Android/src/main/java/com/alarm3d/alarm/AlarmRescheduler.java
+++ Alarm3D-Unity-Android/Assets/Plugins/Android/src/main/java/com/alarm3d/alarm/AlarmRescheduler.java
@@ -15,8 +15,7 @@ public final class AlarmRescheduler
             return;
         }
 
-        // Re-scheduling will be connected to the persistent Unity alarm store.
-        // This entry point is intentionally kept safe until that integration
-        // is implemented.
+        AlarmSchedulerBridge.rescheduleAll(
+                context.getApplicationContext());
     }
 }
